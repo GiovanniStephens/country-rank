f779c227328f30a4ccca70ab2f84baa1230a107b
stephens <giovanni.stephens@trustpower.co.nz>
2020-10-19 16:01:39 +1300

    Started thinking about how to get and estimate of cost of living by country.

---
+++ scrape_cost_of_living.py
@@ -0,0 +1,12 @@
+# Scrape cost of living by country
+
+# Polution rating
+# Crime rating
+# Healthcare rating
+
+# Scrape all countries
+
+# I would like to simulate the cost of living using Monte Carlo Simulation. 
+# We will have to use Triangular distributions
+
+import scrape_urls
