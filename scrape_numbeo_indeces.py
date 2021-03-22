6f1233ef4981011bb4c5b9c3a59393261058209f
Giovanni-Stephens <Giovanni.stephens@trustpower.co.nz>
2021-03-22 16:53:53 +1300

    Added a more dynamic function to get the index I want from Numbeo.

---
+++ scrape_numbeo_indeces.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup  
+import scrape_urls
+import pandas as pd
+
+def to_pandas_df(rows):
+    table = []
+    headers = []
+    for element in rows[0].find_all('th'):
+        headers.append(element.text)
+    table.append(headers)
+
+    for row in rows[1:]:
+        row_elements = []
+        for cell in row.find_all('td'):
+            row_elements.append(cell.text)
+        table.append(row_elements)
+    return pd.DataFrame(table[1:],columns=table[0])
+
+def scrape_index(url = 'https://www.numbeo.com/pollution/rankings_by_country.jsp', \
+    columns = ['Country', 'Pollution']):
+    soup = scrape_urls.scrape_page(url)
+    table = soup.find(lambda tag: tag.name=='table' and tag.has_attr('id') and tag['id']=="t2")
+    rows = table.findAll(lambda tag: tag.name=='tr')
+    table = to_pandas_df(rows)
+    table = table[columns]
+    table.to_csv(f'{columns[-1].split()[0]} by Country.csv', index=False)
+
+if __name__ == '__main__':
+    scrape_index('https://www.numbeo.com/pollution/rankings_by_country.jsp', ['Country', 'Pollution Index'])
+    scrape_index('https://www.numbeo.com/health-care/rankings_by_country.jsp', ['Country', 'Health Care Index'])
+    scrape_index('https://www.numbeo.com/crime/rankings_by_country.jsp', ['Country', 'Crime Index', 'Safety Index'])
