be5ba7bc373360f6732c9192ef0f6092872f82b3
Giovanni-Stephens <stephens.giovanni@gmail.com>
2021-11-14 09:13:17 +1300

    Commenting and linting.

--- EDA.py
+++ EDA.py
@@ -2,10 +2,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import hdbscan
-from sklearn.manifold import TSNE
+# from sklearn.manifold import TSNE
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import KNNImputer
 import estimate_cost_to_retire as retire
+import clustering
 
 df = pd.read_csv('data/All Data by Country.csv')
 
@@ -13,7 +14,7 @@ df = pd.read_csv('data/All Data by Country.csv')
 df.count()
 
 # Cleaned data to keep just the countries with enough data.
-df = df[df.count(1)>=9]
+df = df[df.count(1) >= 9]
 countries = df['Country']
 df = df.drop(['pop2019', 'Crime Index', 'Country'], axis=1)
 
@@ -32,29 +33,41 @@ plt.clf()
 
 # Correlation heatmap
 corr = no_nulls.corr()
-sns.heatmap(no_nulls.corr()[(corr >= 0.4) | (corr <= -0.4)], 
+sns.heatmap(no_nulls.corr()[(corr >= 0.4) | (corr <= -0.4)],
             cmap='viridis', vmax=1.0, vmin=-1.0, linewidths=0.1,
             annot=True, annot_kws={"size": 8}, square=True)
 plt.savefig("visualisations/correlations_heatmap.png")
 plt.clf()
 
 # Regression plots for some of the stronger correlations.
-sns.regplot(x=no_nulls['avg temp'],y='Pollution Index', data=no_nulls)
+sns.regplot(x=no_nulls['avg temp'],
+            y='Pollution Index',
+            data=no_nulls)
 plt.savefig("visualisations/avg_temp_vs_pollution.png")
 plt.clf()
-sns.regplot(x=no_nulls['Cost of Living pw'],y='Pollution Index', data=no_nulls)
+sns.regplot(x=no_nulls['Cost of Living pw'],
+            y='Pollution Index',
+            data=no_nulls)
 plt.savefig("visualisations/cost_vs_pollution.png")
 plt.clf()
-sns.regplot(x=no_nulls['Health Care Index'],y='Pollution Index', data=no_nulls)
+sns.regplot(x=no_nulls['Health Care Index'],
+            y='Pollution Index',
+            data=no_nulls)
 plt.savefig("visualisations/health_care_vs_pollution.png")
 plt.clf()
-sns.regplot(x=no_nulls['Safety Index'],y='Pollution Index', data=no_nulls)
+sns.regplot(x=no_nulls['Safety Index'],
+            y='Pollution Index',
+            data=no_nulls)
 plt.savefig("visualisations/safety_vs_pollution.png")
 plt.clf()
-sns.regplot(x=no_nulls['min temp'],y='avg rainfall', data=no_nulls)
+sns.regplot(x=no_nulls['min temp'],
+            y='avg rainfall',
+            data=no_nulls)
 plt.savefig("visualisations/min_temp_vs_rainfall.png")
 plt.clf()
-sns.regplot(x=no_nulls['Cost of Living pw'],y='Health Care Index', data=no_nulls)
+sns.regplot(x=no_nulls['Cost of Living pw'],
+            y='Health Care Index',
+            data=no_nulls)
 plt.savefig("visualisations/cost_vs_health_care.png")
 plt.clf()
 
@@ -62,10 +75,9 @@ plt.clf()
 std_values = StandardScaler().fit_transform(no_nulls)
 
 # Reduce the dimensionality to 2 dimensions
-import clustering
 # tsne = TSNE(n_components=2, perplexity=10, n_iter=4000)
 # tsne_results = tsne.fit_transform(std_values)
-tsne_results = clustering.reduce_dimensions_umap(std_values,5)
+tsne_results = clustering.reduce_dimensions_umap(std_values, 5)
 
 # Cluster the data
 clusterer = hdbscan.HDBSCAN(min_cluster_size=2)
@@ -81,20 +93,35 @@ labels = clusterer.labels_
 
 # Outliers
 threshold = pd.Series(clusterer.outlier_scores_).quantile(0.9)
-print(countries[clusterer.outlier_scores_>threshold])
+print(countries[clusterer.outlier_scores_ > threshold])
 
-# NZ Cluster (you may need to change the cluster number to work out which one you like)
+# NZ Cluster (you may need to change the cluster
+# number to work out which one you like)
 for i in range(labels.max()):
     print(f'Cluster {i}:')
-    print(pd.merge(no_nulls[labels==i], countries.reset_index(), left_index=True, right_index=True))
+    print(pd.merge(no_nulls[labels == i],
+                   countries.reset_index(),
+                   left_index=True,
+                   right_index=True))
 
 no_nulls['Country'] = countries.reset_index()['Country']
 no_nulls['Cluster'] = labels
 
-potential_retirement_countries = ['Colombia', 'Spain', 'Portugal', 'Greece', 'South Korea', 'New Zealand', 'Georgia', 'Uruguay']
-preferred_clusters = set(no_nulls[no_nulls['Country'].isin(potential_retirement_countries)]['Cluster'].values)
+potential_retirement_countries = ['Colombia',
+                                  'Spain',
+                                  'Portugal',
+                                  'Greece',
+                                  'South Korea',
+                                  'New Zealand',
+                                  'Georgia',
+                                  'Uruguay']
+preferred_clusters = set(no_nulls[no_nulls['Country'].isin(
+    potential_retirement_countries)]['Cluster'].values)
 for preferred_cluster in preferred_clusters:
-    for country in no_nulls[no_nulls['Cluster']==preferred_cluster].values:
-        retirement_cost = retire.estimate_cost_to_retire(country[16],country[4],buffer_pa=0)
+    for country in no_nulls[no_nulls['Cluster'] == preferred_cluster].values:
+        retirement_cost = retire.estimate_cost_to_retire(country[16],
+                                                         country[4],
+                                                         buffer_pa=0)
         country_name = country[16]
-        print(f'The cost to retire in {country_name} is: {round(retirement_cost,2)}')
+        print(f'The cost to retire in \
+              {country_name} is: {round(retirement_cost, 2)}')
