050453def2ceaaf4ec60c376abe84bcfd3debd00
Giovanni-Stephens <stephens.giovanni@gmail.com>
2021-11-14 08:54:31 +1300

    Commenting and linting.

--- predict_PPP.py
+++ predict_PPP.py
@@ -3,13 +3,15 @@ import pandas as pd
 import pycountry
 
 # Import the data
-conversion_rates = pd.read_csv('data/Purchasing Power Parity Conversion Rates.csv')
+conversion_rates = pd.read_csv(
+    'data/Purchasing Power Parity Conversion Rates.csv')
 
 # Clean up the data
 std_country_names = []
 for country in conversion_rates['Country Name']:
     try:
-        std_country_names.append(pycountry.countries.search_fuzzy(country)[0].name)
+        std_country_names.append(
+            pycountry.countries.search_fuzzy(country)[0].name)
     except:
         std_country_names.append(country)
 conversion_rates['std country names'] = std_country_names
@@ -17,32 +19,47 @@ conversion_rates = conversion_rates.set_index('std country names')
 conversion_rates = conversion_rates.drop('Country Name', axis=1)
 
 # Drop countries that do not have enough conversion rates
-filtered_conversion_rates = conversion_rates[conversion_rates.count(1)>=20]
+filtered_conversion_rates = conversion_rates[conversion_rates.count(1) >= 20]
 
 
-def estimate_PPP_conversion_rate_long_term_change(country):
-    # Take an input country
+def estimate_PPP_conversion_rate_long_term_change(country: str) -> float:
+    """
+    Estimate the long term change in the PPP conversion rate of a country.
+
+    :param country: The country to estimate the long term
+                    change in the PPP conversion rate of.
+    :return: The long term change in the PPP conversion rate of the country.
+    """
+    # Try to standardize the country name.
     try:
         std_country = pycountry.countries.search_fuzzy(country)[0].name
     except:
         std_country = country
+    # Get the country's historical conversion rates.
+    ts = filtered_conversion_rates[
+        filtered_conversion_rates.index == std_country]
 
-    ts = filtered_conversion_rates[filtered_conversion_rates.index == std_country]
-
-    if ts.shape[0]>0:
+    if ts.shape[0] > 0:
         # Fit a model
         ts = ts.iloc[-1].dropna()
-        autoarima_model = pmd.auto_arima(ts.transpose(),start_p=0,start_q=0,test="adf",trace=False)
+        autoarima_model = pmd.auto_arima(ts.transpose(),
+                                         start_p=0,
+                                         start_q=0,
+                                         test="adf",
+                                         trace=False)
 
+        # Number of years to predict ahead.
         n_periods = 10
 
-        # forecast the PPP conversion rate
+        # forecast the PPP conversion rate.
         prediction = autoarima_model.predict(n_periods)
 
         # Return the long-term average
         return (prediction[0]/prediction[-1])**(1/n_periods) - 1
     else:
-        return 0 # No result.
+        # No result.
+        return 0
+
 
 if __name__ == '__main__':
     print(estimate_PPP_conversion_rate_long_term_change('South Korea'))
