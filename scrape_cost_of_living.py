c1ae56b41b1e8d32bede803135cf508e6d79aa9a
Giovanni-Stephens <stephens.giovanni@gmail.com>
2021-11-14 08:49:02 +1300

    Commenting and linting.

--- scrape_cost_of_living.py
+++ scrape_cost_of_living.py
@@ -1,19 +1,3 @@
-# Scrape cost of living by country
-
-"""
-To-do:
-1. Create some unit tests to ensure that it is working.
-I would like to be able to run the tests to know that all the formatting and everything is
-still working. It needs to catch if the web page format changes or if there are errors. 
-
-2. Check what countries I am missing from Numbeo and investigate. 
-
-3. Identify what data may be missing. Some countries may be of interest, but I may not be getting info on it.
-
-
-Something is wrong with get_cost_of_living. I am getting super high values. (Should be fixed)
-Also the matching of countries to cost of living is wrong. (Should be fixed)
-"""
 import scrape_urls
 import pandas as pd
 import numpy as np
@@ -22,12 +6,12 @@ import pycountry
 cost_of_living_units = pd.read_csv('data/Cost of Living Items.csv')
 climate_data = pd.read_csv('data/Climate by Country.csv')
 
-def clean_numbeo_table(numbeo_df):
+
+def clean_numbeo_table(numbeo_df: pd.DataFrame) -> pd.DataFrame:
     """
     Cleans the default Numbeo cost of living table.
 
     :numbeo_df: pandas dataframe
-
     :return: pandas dataframe that has been cleaned up.
     """
     # Promote first column to index
@@ -36,30 +20,42 @@ def clean_numbeo_table(numbeo_df):
     numbeo_df = numbeo_df[~numbeo_df[numbeo_df.columns[0]].isnull()]
     # Replace empty strings with nan
     numbeo_df = numbeo_df.replace(r'^\s*$', np.NaN, regex=True)
-    # Replace '?' with NaN 
+    # Replace '?' with NaN
     numbeo_df = numbeo_df.replace(r'^\?$', np.NaN, regex=True)
     # Split range
     try:
-        numbeo_df[['lower', 'upper']] = numbeo_df['Range'].str.split('-', expand = True)
+        numbeo_df[['lower', 'upper']] = numbeo_df['Range'].str.split('-',
+                                                                     expand=True)
     except:
         numbeo_df['lower'] = np.NaN
         numbeo_df['upper'] = np.NaN
         numbeo_df = numbeo_df.astype(str)
-    
+
     # Remove currency 'NZ$'
     numbeo_df['mode'] = numbeo_df['Edit'].str[:-5]
-    numbeo_df.index.rename('category', inplace = True)
-    numbeo_df['lower'] = pd.to_numeric(numbeo_df['lower'].str.replace(',',''), errors='coerce')
-    numbeo_df['mode'] = pd.to_numeric(numbeo_df['mode'].str.replace(',',''), errors='coerce')
-    numbeo_df['upper'] = pd.to_numeric(numbeo_df['upper'].str.replace(',',''), errors='coerce')
+    numbeo_df.index.rename('category', inplace=True)
+    numbeo_df['lower'] = pd.to_numeric(numbeo_df['lower'].str.replace(',', ''),
+                                       errors='coerce')
+    numbeo_df['mode'] = pd.to_numeric(numbeo_df['mode'].str.replace(',', ''),
+                                      errors='coerce')
+    numbeo_df['upper'] = pd.to_numeric(numbeo_df['upper'].str.replace(',', ''),
+                                       errors='coerce')
     return numbeo_df
 
 
-def get_cost_of_living(numbeo_table, simulations = 20000, percentile = 90):
+def get_cost_of_living(numbeo_table: pd.DataFrame,
+                       simulations: int = 20000,
+                       percentile: int = 90) -> float:
     """
-    For all cost categories, get the cost and multiply it by the number of units. 
-    I simulate the cost using a triangular distribution if there is a lower and upper bound. 
+    For all cost categories, get the cost and multiply
+    it by the number of units. I simulate the cost using a
+    triangular distribution if there is a lower and upper bound.
     If there isn't one though, I simply take the mode.
+
+    :numbeo_table: pandas dataframe with the cost of living.
+    :simulations: number of simulations to run.
+    :percentile: percentile to use.
+    :return: cost of living in the input country.
     """
     vals = np.zeros(simulations)
     for i in range(len(cost_of_living_units) - 1):
@@ -71,7 +67,10 @@ def get_cost_of_living(numbeo_table, simulations = 20000, percentile = 90):
             mode = numbeo_table.loc[category]['mode']
             upper = numbeo_table.loc[category]['upper']
             if lower > 0 and upper > 0 and mode > 0:
-                vals = np.add(vals, units * np.random.triangular(lower, mode, upper, simulations))
+                vals = np.add(vals, units * np.random.triangular(lower,
+                                                                 mode,
+                                                                 upper,
+                                                                 simulations))
             else:
                 vals = np.add(vals, [mode*units]*simulations)
         except:
@@ -79,52 +78,65 @@ def get_cost_of_living(numbeo_table, simulations = 20000, percentile = 90):
     return np.percentile(vals, percentile)
 
 
-def check_enough_data(numbeo_df):
+def check_enough_data(numbeo_df: pd.DataFrame) -> float:
     """
-    Checks that the number of data points we have is sufficient. 
-    I check that I have enough data to be able to use it to estimate cost of living.
+    Checks that the number of data points we
+    have is sufficient.
+    I check that I have enough data to be able
+    to use it to estimate cost of living.
 
-    :numbeo_df: pandas dataframe with an '[ Edit ]' column.
-
-    :return: proportion of filled cells as a proportion of number of total categories.
+    :numbeo_df: pandas dataframe.
+    :return: proportion of filled cells as a proportion of
+             number of total categories.
     """
     if type(numbeo_df) == list:
         return 0
     else:
-        categories = cost_of_living_units[cost_of_living_units['Units pw'] > 0]['Category']
+        categories = cost_of_living_units[
+                     cost_of_living_units['Units pw'] > 0]['Category']
         intersecting_categories = numbeo_df.index.intersection(categories)
         num_nulls = numbeo_df.loc[intersecting_categories]['Edit'].isna().sum()
         return (len(intersecting_categories) - num_nulls) / len(categories)
 
+
 def get_numbeo_countries():
+    """
+    This function returns a list of countries
+    that have been scraped from Numbeo.
+    The countries get standardized using the pycountry library.
+    """
     soup = scrape_urls.scrape_page('https://www.numbeo.com/cost-of-living')
     table = soup.find_all(class_='related_links')
     countries = table[0].find_all('a')
     std_countries = {}
     for i in range(len(countries)):
         try:
-            std_countries[countries[i].text] = pycountry.countries.search_fuzzy(countries[i].text)[0].name
+            std_countries[countries[i].text] = pycountry.countries.search_fuzzy(
+                                               countries[i].text)[0].name
         except:
             pass
     return std_countries
 
+
 def main():
     urls = []
-    # Create a url for each country based on the temperature website country names (there may be some differences)
+    # Create a url for each country based on the temperature
+    # website country names (there may be some differences).
     countries = get_numbeo_countries()
     for country in countries.keys():
         country_str = '+'.join(country.title().split())
         urls.append(f'https://www.numbeo.com/cost-of-living/country_result.jsp?country={country_str}&displayCurrency=NZD')
-    
+
     # Scrape the pages
-    #soups = scrape_urls.multi_thread_func(scrape_urls.scrape_page, urls)
+    # soups = scrape_urls.multi_thread_func(scrape_urls.scrape_page, urls)
     soups = [scrape_urls.scrape_page(url) for url in urls]
-    
+
     # Extract the data tables
     tables = [scrape_urls.get_table(soup, 1, 0, -1) for soup in soups]
-    
+
     # Clean and format the tables
-    cleaned_tables = [clean_numbeo_table(table) if type(table) != list else [] for table in tables]
+    cleaned_tables = [clean_numbeo_table(table)
+                      if type(table) != list else [] for table in tables]
 
     # Get cost of living for every country if there is enough data.
     cost_of_living_dic = {}
@@ -133,16 +145,21 @@ def main():
             cost_of_living_dic[country] = get_cost_of_living(table)
         else:
             cost_of_living_dic[country] = ''
-    
+
     # Format results table and save
     df = pd.DataFrame.from_dict(cost_of_living_dic, orient='index')
     df.index.rename('Country', inplace=True)
     df.columns = ['Cost of Living pw']
     df.to_csv('data/Cost of Living by Country.csv')
 
-def get_city_cost_of_living(city, percentile = 90):
+
+def get_city_cost_of_living(city: str, percentile: int = 90) -> float:
     """
     Get the cost of living for a city.
+
+    :city: city to get the cost of living for.
+    :percentile: percentile to use.
+    :return: cost of living for the city.
     """
     city = '+'.join(city.title().split())
     url = f'https://www.numbeo.com/cost-of-living/in/{city}?displayCurrency=NZD'
@@ -151,23 +168,33 @@ def get_city_cost_of_living(city, percentile = 90):
     cleaned_table = clean_numbeo_table(table)
     # check if there is enough data
     if check_enough_data(cleaned_table) > 0.9:
-        print(str(percentile)+ 'th percentile weekly cost of living in ' + city + ': ' + \
-             str(round(get_cost_of_living(cleaned_table, percentile=percentile),2)))
+        print(str(percentile) + 'th percentile weekly cost of living in ' + city + ': ' +
+              str(round(get_cost_of_living(cleaned_table, percentile=percentile), 2)))
     else:
         print('Not enough data to estimate cost of living.')
 
 
-def get_country_cost_of_living(country, percentile):
+def get_country_cost_of_living(country: str, percentile: int = 90) -> float:
+    """
+    Get the cost of living for a country.
+
+    :country: country to get the cost of living for.
+    :percentile: percentile to use.
+    :return: cost of living for the country.
+    """
     country_str = '+'.join(country.title().split())
     url = f'https://www.numbeo.com/cost-of-living/country_result.jsp?country={country_str}&displayCurrency=NZD'
     soup = scrape_urls.scrape_page(url)
     table = scrape_urls.get_table(soup, 1, 0, -1)
     cleaned_table = clean_numbeo_table(table)
     if check_enough_data(cleaned_table) > 0.9:
-        print(str(percentile)+ 'th percentile weekly cost of living in ' + country + ': ' + \
-             str(round(get_cost_of_living(cleaned_table, percentile=percentile),2)))
+        print(str(percentile) + 'th percentile weekly cost of living in '
+              + country + ': ' +
+              str(round(get_cost_of_living(cleaned_table,
+                                           percentile=percentile), 2)))
     else:
         print('Not enough data.')
+
 
 if __name__ == "__main__":
     # get_country_cost_of_living('colombia', 99)
