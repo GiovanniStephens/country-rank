fe1dec61a32e0a7843571fe9fde94b67866b3d72
Giovanni-Stephens <stephens.giovanni@gmail.com>
2021-11-14 08:32:08 +1300

    Commenting and linting.

--- scrape_numbeo_indeces.py
+++ scrape_numbeo_indeces.py
@@ -1,8 +1,14 @@
-from bs4 import BeautifulSoup  
 import scrape_urls
 import pandas as pd
 
+
 def to_pandas_df(rows):
+    """
+    Converts a list of HTML rows to a pandas dataframe.
+
+    :rows: list of rows
+    :return: pandas dataframe
+    """
     table = []
     headers = []
     for element in rows[0].find_all('th'):
@@ -14,18 +20,31 @@ def to_pandas_df(rows):
         for cell in row.find_all('td'):
             row_elements.append(cell.text)
         table.append(row_elements)
-    return pd.DataFrame(table[1:],columns=table[0])
+    return pd.DataFrame(table[1:], columns=table[0])
 
-def scrape_index(url = 'https://www.numbeo.com/pollution/rankings_by_country.jsp', \
-    columns = ['Country', 'Pollution']):
+
+def scrape_index(url='https://www.numbeo.com/pollution/rankings_by_country.jsp',
+                 columns=('Country', 'Pollution')):
+    """
+    Scrapes the pollution index from the numbeo website.
+
+    :url: url to scrape.
+    :columns: columns to scrape.
+    :return: pandas dataframe.
+    """
     soup = scrape_urls.scrape_page(url)
-    table = soup.find(lambda tag: tag.name=='table' and tag.has_attr('id') and tag['id']=="t2")
-    rows = table.findAll(lambda tag: tag.name=='tr')
+    table = soup.find(lambda tag: tag.name == 'table'
+                      and tag.has_attr('id') and tag['id'] == "t2")
+    rows = table.findAll(lambda tag: tag.name == 'tr')
     table = to_pandas_df(rows)
     table = table[columns]
     table.to_csv(f'{columns[-1].split()[0]} by Country.csv', index=False)
 
+
 if __name__ == '__main__':
-    scrape_index('https://www.numbeo.com/pollution/rankings_by_country.jsp', ['Country', 'Pollution Index'])
-    scrape_index('https://www.numbeo.com/health-care/rankings_by_country.jsp', ['Country', 'Health Care Index'])
-    scrape_index('https://www.numbeo.com/crime/rankings_by_country.jsp', ['Country', 'Crime Index', 'Safety Index'])
+    scrape_index('https://www.numbeo.com/pollution/rankings_by_country.jsp',
+                 ['Country', 'Pollution Index'])
+    scrape_index('https://www.numbeo.com/health-care/rankings_by_country.jsp',
+                 ['Country', 'Health Care Index'])
+    scrape_index('https://www.numbeo.com/crime/rankings_by_country.jsp',
+                 ['Country', 'Crime Index', 'Safety Index'])
